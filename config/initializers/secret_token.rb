2eb60f8c886eea30e72fdf2fd56934bf01d05213
Seth Epstein <seth.a.epstein@gmail.com>
2012-11-03 15:33:13 -0500

    Primary Commit CAP3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cap3::Application.config.secret_token = '15a6c90c5a4b960329911120e6722bbac87d17d6122863a83e2cb1bb17980fa98e5bf41efb7f93e78aa1065e9004b4555eb7fe58f48bc0eaafafb752fc3420be'
